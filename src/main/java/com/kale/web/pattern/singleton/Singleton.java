c01caf5ac7fa01530f5758bec873900d7f507dfe
kaleLiu <805654685@qq.com>
2017-03-26 17:42:40 +0800

    单例模式

--- src/main/java/com/kale/web/pattern/singleton/Singleton.java
+++ src/main/java/com/kale/web/pattern/singleton/Singleton.java
@@ -1,7 +1,38 @@
 package com.kale.web.pattern.singleton;
 
+import java.util.Vector;
+
 /**
  * Created by kaleLiu on 2017/3/26 0026.
  */
 public class Singleton {
+
+    private static Singleton instance = null;
+
+    private Vector properties = null;
+
+    public Vector getProperties() {
+        return properties;
+    }
+
+    private Singleton() {
+    }
+
+    private static synchronized void syncInit() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            syncInit();
+        }
+        return instance;
+    }
+
+    public void updateProperties() {
+        Singleton shadow = new Singleton();
+        properties = shadow.getProperties();
+    }
 }
